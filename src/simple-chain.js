691d0debc837e3f63586990ddeb42e20fb94582e
RybalkoYakov <RybalkoYakovv@gmail.com>
2023-04-16 16:38:04 +0300

    feat: chainMaker complete

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -8,26 +8,40 @@ const chainMaker = {
   chain: [],
 
   getLength() {
-    return this.chain.length
+    return this.chain.length;
   },
-  addLink( value ) {
-    this.chain.push(`( ${value} )`)
-    return this
-  },
-  removeLink( position ) {
-    if (position <= 0 || position > this.chain.length || typeof position !== "number") throw new Error("You can't remove incorrect link!")
 
-    this.chain.splice(position  - 1, 1)
-    return this
+
+  addLink(value) {
+    if (arguments.length === 0) {
+      value = '';
+    }
+    this.chain.push(`( ${value} )`);
+    return this;
   },
+
+  removeLink(position) {
+    if (
+      typeof position !== 'number' ||
+      position < 1 ||
+      position > this.chain.length
+    ) {
+      this.chain = [];
+      throw new Error('You can\'t remove incorrect link!');
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
+  },
+
   reverseChain() {
-    this.chain.reverse()
-    return this
+    this.chain.reverse();
+    return this;
   },
+
   finishChain() {
-    let tmp = this.chain
-    this.chain = []
-    return tmp.join('~~')
+    const result = this.chain.join('~~');
+    this.chain = [];
+    return result;
   }
 };
 
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,61 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let encryptedString = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      let charCode = message.charCodeAt(i);
+
+      if (charCode >= 65 && charCode <= 90) {
+
+        let keyCharCode = key.charCodeAt(keyIndex % key.length);
+
+        encryptedString += String.fromCharCode(((charCode - 65 + keyCharCode - 65) % 26) + 65);
+        keyIndex++;
+      } else {
+        encryptedString += String.fromCharCode(charCode);
+      }
+    }
+
+    return this.direct ? encryptedString : encryptedString.split('').reverse().join('');
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let decryptedString = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      let encryptedChar = message.charCodeAt(i);
+
+      if (encryptedChar >= 65 && encryptedChar <= 90) {
+        let keyChar = key.charCodeAt(keyIndex % key.length);
+        decryptedString += String.fromCharCode(((encryptedChar - 65 - keyChar + 91) % 26) + 65);
+        keyIndex++;
+      } else {
+        decryptedString += String.fromCharCode(encryptedChar);
+      }
+    }
+
+    return this.direct ? decryptedString : decryptedString.split('').reverse().join('');
   }
 }
+
 
 module.exports = {
   VigenereCipheringMachine
